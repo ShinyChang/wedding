40865bf1cde5147ac74db35de3e54e80f68def3f
ZhiHong Chang <ublue724@gmail.com>
2015-02-02 23:56:44 +0800

    V2 FHD view

---
+++ js/main2.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+// Google Analytics
+window.GoogleAnalyticsObject = 'ga';
+window.ga = window.ga || function() {
+    (window.ga.q = window['ga'].q || []).push(arguments)
+};
+ga('create', 'UA-27224084-4', 'auto');
+ga('send', 'pageview');
