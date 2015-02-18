93c93e5a5a554c32d50f3adb0735214eb2b61cd7
ZhiHong Chang <ublue724@gmail.com>
2015-02-18 20:10:49 +0800

    bug fixed

--- js/reward.js
+++ js/reward.js
@@ -1,7 +1,7 @@
 "use strict";
 (function(){
     $.get("http://wedding.shinychang.net/reward", function(o){
-        $container = $(".container");
+        var $container = $("body > div.container");
         for (var i = o.length - 1; i >= 0; i--) {
             var template = "<div class='media'>"
                                 + "<div class='media-left'>"
@@ -9,6 +9,7 @@
                                 + "</div>"
                                 + "<div class='media-body'>"
                                     + "<h4 class='media-heading'>" + o[i].name + "</h4>"
+                                    + o[i].message
                                 + "</div>"
                             + "</div>";
             $container.append(template);
