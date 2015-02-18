e5485c7f9c5ff81eb18c193cff2c9ae1564b0327
ZhiHong Chang <ublue724@gmail.com>
2015-02-18 20:07:33 +0800

    reward

---
+++ js/reward.js
@@ -0,0 +1,17 @@
+"use strict";
+(function(){
+    $.get("http://wedding.shinychang.net/reward", function(o){
+        $container = $(".container");
+        for (var i = o.length - 1; i >= 0; i--) {
+            var template = "<div class='media'>"
+                                + "<div class='media-left'>"
+                                    + "<div class='profile' style='background-image: url(https://graph.facebook.com/"+ o[i].id + "/picture?type=large)'></div>"
+                                + "</div>"
+                                + "<div class='media-body'>"
+                                    + "<h4 class='media-heading'>" + o[i].name + "</h4>"
+                                + "</div>"
+                            + "</div>";
+            $container.append(template);
+        };
+    }, 'json');
+})();
